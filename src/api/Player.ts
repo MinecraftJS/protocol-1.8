9d8ec6b05b6895f1543c347c2b24bc61e23cab49
RichardDorian <richard.dorian@outlook.com>
2022-12-06 08:25:03 +0100

    feat: :construction: add methods to player class

--- src/api/Player.ts
+++ src/api/Player.ts
@@ -1,11 +1,15 @@
-import { StringComponentBuilder } from '@minecraft-js/chat';
+import {
+  ComponentResolvable,
+  StringComponentBuilder,
+} from '@minecraft-js/chat';
 import {
   ChangeGameStateGameMode,
   ChangeGameStateReason,
   ChatPosition,
+  Particle,
+  Position,
 } from '../protocol';
 import { MinecraftServerClient } from '../server/Client';
-import exp = require('constants');
 
 /**
  * ⚠️ This Player class is experimental
@@ -15,6 +19,9 @@ import exp = require('constants');
 export class Player {
   private _gamemode: ChangeGameStateGameMode;
   private _level: number;
+  private _health: number;
+  private _food: number;
+  private _foodSaturation: number;
 
   private readonly client: MinecraftServerClient;
 
@@ -45,6 +52,7 @@ export class Player {
     }
   }
 
+  /** The player's current gamemode */
   get gamemode() {
     return this._gamemode;
   }
@@ -60,6 +68,16 @@ export class Player {
     this.client.writeRaw(packet);
   }
 
+  /**
+   * The player's current level.
+   * Minecraft leveling system is weird
+   * but the maths are done for you.
+   * @example
+   * ```js
+   * // Set the level to 2 and the progression bar to a half
+   * player.level = 2.5;
+   * ```
+   */
   get level() {
     return this._level;
   }
@@ -81,6 +99,157 @@ export class Player {
       experienceBar: value - level,
       level,
       totalExperience: experience,
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  /** The player's health, varies from 0 to 20 (half hearts). If the value is 0 or less the player will be killed */
+  get health() {
+    return this._health;
+  }
+
+  set health(value: number) {
+    if (value > 20) throw new Error("Health value can't be greater than 20");
+
+    this._health = value;
+
+    const packet = this.client.packetWriter.write('UpdateHealthPacket', {
+      health: value,
+      food: this.food,
+      foodSaturation: this.foodSaturation,
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  /** The player's food bar */
+  get food() {
+    return this._food;
+  }
+
+  set food(value: number) {
+    if (value > 20) throw new Error("Food value can't be greater than 20");
+
+    this._food = value;
+
+    const packet = this.client.packetWriter.write('UpdateHealthPacket', {
+      health: this.health,
+      food: value,
+      foodSaturation: this.foodSaturation,
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  /** The player's saturation bar */
+  get foodSaturation() {
+    return this._foodSaturation;
+  }
+
+  set foodSaturation(value: number) {
+    if (value > 5)
+      throw new Error("Food saturation value can't be greater than 5");
+
+    this._foodSaturation = value;
+
+    const packet = this.client.packetWriter.write('UpdateHealthPacket', {
+      health: this.health,
+      food: this.food,
+      foodSaturation: value,
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  /**
+   * Kick the player and sends them back to the multiplayer screen.
+   * `Disconnected` will be shown on the user's screen.
+   */
+  public kick(): void;
+  /**
+   * Kick the player and sends them back to the multiplayer screen
+   * @param message The message that'll be displayed on the user's screen
+   */
+  public kick(message: string): void;
+  /**
+   * Kick the player and sends them back to the multiplayer screen
+   * @param component The chat component that'll be displayed on the user's screen
+   */
+  public kick(component: ComponentResolvable): void;
+  public kick(arg1?: string | ComponentResolvable): void {
+    this.client.disconnect(arg1);
+  }
+
+  public displayParticle(
+    particle: Particle,
+    location: Position,
+    data?: number[]
+  ): void;
+  public displayParticle(
+    particle: Particle,
+    location: Position,
+    count: number,
+    data?: number[],
+    longDistance?: boolean
+  ): void;
+  public displayParticle(
+    particle: Particle,
+    location: Position,
+    arg1: number[] | number,
+    data?: number[],
+    longDistance?: boolean
+  ): void {
+    if (Array.isArray(arg1)) {
+      data = arg1;
+      arg1 = 1; // Default value
+    }
+
+    const packet = this.client.packetWriter.write('ParticlePacket', {
+      particleId: particle,
+      longDistance: longDistance ?? false,
+      ...location,
+      offsetX: 0,
+      offsetY: 0,
+      offsetZ: 0,
+      particleData: 0,
+      particleCount: arg1,
+      data: data ?? [],
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  public registerPluginChannel(channel: string): void;
+  public registerPluginChannel(channels: string[]): void;
+  public registerPluginChannel(arg1: string | string[]): void {
+    if (typeof arg1 === 'string') arg1 = [arg1];
+
+    const packet = this.client.packetWriter.write('PluginMessagePacket', {
+      channel: 'REGISTER',
+      data: Buffer.from(arg1.join('\0') + '\0'),
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  public unregisterPluginChannel(channel: string): void;
+  public unregisterPluginChannel(channels: string[]): void;
+  public unregisterPluginChannel(arg1: string | string[]): void {
+    if (typeof arg1 === 'string') arg1 = [arg1];
+
+    const packet = this.client.packetWriter.write('PluginMessagePacket', {
+      channel: 'UNREGISTER',
+      data: Buffer.from(arg1.join('\0') + '\0'),
+    });
+
+    this.client.writeRaw(packet);
+  }
+
+  public sendPluginMessage(channel: string, message: Buffer): void {
+    const packet = this.client.packetWriter.write('PluginMessagePacket', {
+      channel,
+      data: message,
     });
 
     this.client.writeRaw(packet);
